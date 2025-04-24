f1754a09b4dbf073a943a14a9e826eab654777ee
YooSihyeon <qaplsk@gmail.com>
2025-04-24 15:03:44 +0900

    ‘temp’

--- problem4.py
+++ problem4.py
@@ -28,18 +28,18 @@ def problem4 (grid):
     all_paths =[]
 
     #dfs 를 사용해서 위치 탐색, x,y는 그리드의 위치를 의미함.
-    def dfs (x, y) :
+    def dfs (x, y,target) :
     # 범위 밖에 있거나 이미 방문했으면 False 반환
-        if not (0 <= x < grid_n and 0 <= y < grid_n):
-            return False
-        #이미 방문했으면 False 반환
-        if visited[x][y]:
-            return False
-        path.append((x,y)) # path에 경로 저장
-        # 처음 시작 좌표 값이 0이면 True 반환
-        if grid[x][y] == 0:
-            all_paths.append(list(path)) 
-            return True
+        if x < 0 or x >= len(grid) or y < 0 or y >= len(grid[0]) or visited[x][y]:
+            return
+        path.append((x, y))
+        visited[x][y] = True
+        if grid[x][y] == target:
+            # 경로에 해당하는 숫자값들의 합 계산
+            value_sum = sum(grid[i][j] for i, j in path)
+            all_paths.append((list(path), value_sum))  # 경로와 합 저장
+
+        path.pop()
 
         # 방문한 좌표들을 true로 설정 (원래 모두 false 였음)
         visited[x][y] = True 
@@ -48,28 +48,21 @@ def problem4 (grid):
         step = grid[x][y]
 
         # 상하좌우 순으로 탐색, 재귀 함수 호출
-        if (dfs(x - step, y) or
-            dfs(x + step, y) or
-            dfs(x, y - step) or
-            dfs(x, y + step)):
-            return True
+        if (dfs(x - step, y,0) or
+            dfs(x + step, y,0) or
+            dfs(x, y - step,0) or
+            dfs(x, y + step,0)):
+            return all_paths
         # 만약 경로가 아닌 경우 다시 돌아가야함 
         # 실패한 경로는 지워짐. 이전 위치로 돌아가게 됨.
-        path.pop() 
 
-        return False
 
-    if dfs(0, 0):
-        #dfs결과가 true면 yes라고 출력
-        print("YES")
-        # path의 결과들을 route로 연결
-        route = " → ".join(f"({x},{y})" for x, y in path)
-        # 모든 가능한 path출력하기
-        print(all_paths)
-        # 예시 처럼 출력
-        print(f"{route} → 0 도달 → 성공")
-        
-    else: # dfs 결과가 false이면 no라고 출력
-        print("NO")
+        return 
+
+
+
+    return all_paths 
 
 problem4(grid)
+
+
